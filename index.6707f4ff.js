127cc3d2bb1d4d4a38ac63635eba4ac29be19325
ChaikAndrew <101956196+ChaikAndrew@users.noreply.github.com>
2022-10-06 21:13:05 +0000

    Deploying to gh-pages from @ ChaikAndrew/modal_film_info@3c39608c3a680b7955dca911d3151dfe55eec854 🚀

--- index.6707f4ff.js
+++ index.6707f4ff.js
@@ -1,2 +1,2 @@
 !function(){var e=document,t=e.getElementsByClassName("opModal")[0],s=e.getElementsByClassName("film-card__close")[0],a=e.getElementsByClassName("film-card__modal")[0];t.addEventListener("click",(function(){a.classList.toggle("active")})),s.addEventListener("click",(function(){a.classList.remove("active")}))}();
-//# sourceMappingURL=index.39a1e8ae.js.map
+//# sourceMappingURL=index.6707f4ff.js.map
--- index.d0abfb61.js
+++ index.d0abfb61.js
@@ -1,2 +1,2 @@
 const e=document;let s=e.getElementsByClassName("opModal")[0],t=e.getElementsByClassName("film-card__close")[0],l=e.getElementsByClassName("film-card__modal")[0];s.addEventListener("click",(()=>{l.classList.toggle("active")})),t.addEventListener("click",(()=>{l.classList.remove("active")}));
-//# sourceMappingURL=index.063f6f73.js.map
+//# sourceMappingURL=index.d0abfb61.js.map
